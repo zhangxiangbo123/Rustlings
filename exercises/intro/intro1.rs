27b585c0504f31f080fe5daca8ae9c2efaa02344
zhangxiangbo123 <imzhangxiangbo@gmail.com>
2025-01-17 21:11:55 +0800

    update

--- exercises/intro/intro1.rs
+++
@@ -1,5 +0,0 @@
-fn main() {
-    // Congratulations, you finished the first exercise 🎉
-    // As an introduction to Rustlings, the first exercise only required
-    // entering `n` in the terminal to go to the next exercise.
-}
